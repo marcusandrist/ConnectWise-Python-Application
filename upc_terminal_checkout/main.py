acff9947405681449e9e120d71409ecc54c3bf6f
Marcus Andrist <marcus.andrist@drake.edu>
2023-05-17 15:27:00 -0500

    Started working on more wrapper functions.

--- upc_terminal_checkout/main.py
+++ upc_terminal_checkout/main.py
@@ -1,13 +1,13 @@
 import typing
-from PyQt5 import QtCore
 from wrapper import endpoint_get, endpoint_post
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget
 from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import QFile
 
-# Subclass for QMainWindow 
+
+# Subclass for QMainWindow
 class ScannerApp(QMainWindow):
-
     def __init__(self, parent=None):
         super().__init__(parent)
 
@@ -16,14 +16,12 @@ class ScannerApp(QMainWindow):
         self.setWindowIcon(QIcon("tool_checkout.png"))
         self.resize(600, 600)
 
-        # Set CSS (to-do)
-        # self.load_stylesheet('styles.css')
-
         # Execute application inside GUI
         self.app()
 
     def app(self):
         pass
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
--- upc_terminal_checkout/wrapper.py
+++ upc_terminal_checkout/wrapper.py
@@ -1,14 +1,68 @@
 from config import base_URL, headers
-from requests import get, post
+from requests import get, post, patch
+from requests.exceptions import RequestException
+from typing import Dict, List, AnyStr, Optional
+
+
+# helper functions
+# Returns a list of all configurations that match the type "target"
+def config_filter(data: List[Dict], target: AnyStr):
+    filtered_data = [item for item in data if item["type"]["name"] == target]
+    return filtered_data
+
+
+# Structured vanilla http requests
+
 
 # Runs a get on any endpoint
-def endpoint_get(endpoint: str) -> dict:
-    return get(
-        base_URL + endpoint,
-        headers=headers,
-    ).json()
+def endpoint_get(endpoint: AnyStr) -> Dict:
+    # Query String paramater added for max-size results (1,000)
+    return get(base_URL + endpoint + "?pageSize=1000", headers=headers).json()
+
 
 # Accepts an endpoint (format /xxx/xxx) and a dictionary as a json payload
-def endpoint_post(endpoint: str, payload: dict) -> dict:
-    post_response = post(base_URL + endpoint, headers=headers, json=payload)
-    return post_response.json()
+def endpoint_post(endpoint: AnyStr, payload: Dict) -> Dict:
+    try:
+        # Make request to api and possibly have error code
+        response = post(base_URL + endpoint, headers=headers, json=payload)
+        response.raise_for_status()
+
+        return response.json()
+    
+    # Show error stack
+    except RequestException as e:
+        print(f"Request error: {e}")
+    except ValueError as e:
+        print(f"JSON processing error: {e}")
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+
+    return None
+
+
+# Runs a patch on any endpoint
+def endpoint_patch(endpoint: AnyStr, payload: Dict) -> Optional[Dict]:
+    try:
+        # Make request to api and possibly have error code
+        response = patch(base_URL + endpoint, headers=headers, json=payload)
+        response.raise_for_status()
+
+        return response.json()
+    
+    # Show error stack
+    except RequestException as e:
+        print(f"Request error: {e}")
+    except ValueError as e:
+        print(f"JSON processing error: {e}")
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+
+    return None
+
+
+# More heavily parameterized https request & processor methods live here
+
+
+# Returns all the configurations of of type Key
+def fetch_keys() -> List[Dict]:
+    return config_filter(endpoint_get("/company/configurations"), "Key")
